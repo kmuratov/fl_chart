6a5173da49f723140cc050811e4bb8ec45ce0a9b
kmuratov <kirillmailpost@gmail.com>
2022-01-05 12:30:08 +0300

    Hotfix for WWG. The issue described on https://issueexplorer.com/issue/imaNNeoFighT/fl_chart/625#19263423

--- lib/src/chart/base/axis_chart/axis_chart_painter.dart
+++ lib/src/chart/base/axis_chart/axis_chart_painter.dart
@@ -261,8 +261,13 @@ abstract class AxisChartPainter<D extends AxisChartData>
           final y1 = 0 + getTopOffsetDrawSize(holder);
           final x2 = bothX;
           final y2 = usableViewSize.height + getTopOffsetDrawSize(holder);
-          canvasWrapper.drawDashedLine(Offset(x1, y1), Offset(x2, y2),
-              _gridPaint, flLineStyle.dashArray);
+          // https://issueexplorer.com/issue/imaNNeoFighT/fl_chart/625#19263423
+          try {
+            canvasWrapper.drawDashedLine(Offset(x1, y1), Offset(x2, y2),
+                _gridPaint, flLineStyle.dashArray);
+          } catch (e) {
+            continue;
+          }
         }
         verticalSeek += verticalInterval;
       }
@@ -301,8 +306,13 @@ abstract class AxisChartPainter<D extends AxisChartData>
           final y1 = bothY;
           final x2 = usableViewSize.width + getLeftOffsetDrawSize(holder);
           final y2 = bothY;
-          canvasWrapper.drawDashedLine(
-              Offset(x1, y1), Offset(x2, y2), _gridPaint, flLine.dashArray);
+          // https://issueexplorer.com/issue/imaNNeoFighT/fl_chart/625#19263423
+          try {
+            canvasWrapper.drawDashedLine(
+                Offset(x1, y1), Offset(x2, y2), _gridPaint, flLine.dashArray);
+          } catch (e) {
+            continue;
+          }
         }
 
         horizontalSeek += horizontalInterval;
